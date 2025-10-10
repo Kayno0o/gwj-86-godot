d6e31038a8fce197ef58bda96c379ccc337bce98
Kayno0o <kevyn.fyleyssant@gmail.com>
2025-10-11 01:35:23 +0200

    feat: mask methods

--- scripts/characters/mask.gd
+++ scripts/characters/mask.gd
@@ -1,31 +1,29 @@
-@tool
 extends CharacterBody2D
+class_name Mask
 
 @export var mask_task: MaskTask.Type = MaskTask.Type.ChopWood
-@export var sprite_texture: Texture2D
 @export var color: Color = Color.WHITE
 @export var mask_name: String = "Mask"
+@export var movement_speed: float = 50.0
 
-@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
 
 func _ready():
 	apply_mask_properties()
+	abstract_ready()
+
+func abstract_ready() -> void:
+	return
+
+func _physics_process(delta):
+	abstract_physics_process(delta)
+
+func abstract_physics_process(_delta) -> void:
+	return
 
 func apply_mask_properties():
 	if not is_node_ready():
 		return
-
-	if not sprite:
-		return
-
-	if sprite_texture:
-		var sprite_frames = SpriteFrames.new()
-		# sprite_frames.add_animation("default")
-		sprite_frames.add_frame("default", sprite_texture)
-		sprite.sprite_frames = sprite_frames
-
-	sprite.modulate = color
 
 func get_mask_task() -> MaskTask.Type:
 	return mask_task
