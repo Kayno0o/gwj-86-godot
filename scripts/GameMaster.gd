a48418303f33370952b53dcfe6f34e546a2c3090
Atoutain <legastoutaindu27@gmail.com>
2025-10-12 16:19:23 +0200

    feat : spawner finished
    cleanup to do
    Verifier les signaux inutile (midday par exemple)
    Renommer ennemie en villains
    commenter le code pour le rendre comprehensible

--- scripts/GameMaster.gd
+++ scripts/GameMaster.gd
@@ -6,6 +6,7 @@ signal kill_villain()
 
 @export var day_lenght = 5.0
 @export var difficulty : int = 1
+@export var map_size : Array[Vector2]
 
 @onready var is_midnight : bool = false
 
@@ -16,14 +17,21 @@ func _unhandled_input(event):
 		if event.pressed and event.keycode == KEY_W:
 			kill_villain.emit()
 
-
-
-func _on_day_night_cycler_midday() -> void:
+func _place_spawner() :
+	var rand_choice = randi_range(0, 3)
+	var location_choice = Vector2(randf_range(map_size[rand_choice].x, map_size[(rand_choice + 1) % 4].x), randf_range(map_size[rand_choice].y, map_size[(rand_choice + 1) % 4].y))
 	var spawner = preload("res://stages/main/spawner.tscn").instantiate()
+	spawner.position = location_choice
 	add_child(spawner)
 	self.get_child(0).spawner_ded.connect(_spawner_is_ded)
-	spawn_enemy.emit()
 
+func _on_day_night_cycler_midday() -> void:
+	#var spawner = preload("res://stages/main/spawner.tscn").instantiate()
+	#add_child(spawner)
+	#self.get_child(0).spawner_ded.connect(_spawner_is_ded)
+	#_place_spawner()
+	#spawn_enemy.emit()
+	pass
 
 func _on_day_night_cycler_midnight() -> void:
 	is_midnight = true
@@ -37,3 +45,7 @@ func _spawner_is_ded() :
 	if is_midnight == true :
 		is_midnight = false
 		end_night.emit()
+
+func _on_timer_timeout() -> void:
+	_place_spawner()
+	spawn_enemy.emit()
