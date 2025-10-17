19c21aae1b1d44fb24690ad2555349b5fc325bf8
Atoutain <legastoutaindu27@gmail.com>
2025-10-17 15:29:41 +0200

    fix : resize everything
    attach script to order all sprites

--- Ui/entitiespriteordering.gd
+++ Ui/entitiespriteordering.gd
@@ -1,12 +1,12 @@
-extends Sprite2D
+extends AnimatedSprite2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	self.z_index = $totembasesprite.z_index + 1
-	pass
+	self.z_index = self.position.y
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	self.z_index = self.global_position.y
