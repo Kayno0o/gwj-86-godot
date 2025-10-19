ee105b66974f790499a39fd60c6646d866b1f8b0
Kayno0o <kevyn.fyleyssant@gmail.com>
2025-10-19 19:44:02 +0200

    fix: imports

--- props/totem/fire_click.gd
+++ props/totem/fire_click.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton :
-		if event.button_mask == 1 :
+		if event.button_mask == MOUSE_BUTTON_MASK_LEFT:
 			pass #faire apparaitre l'inventaire
