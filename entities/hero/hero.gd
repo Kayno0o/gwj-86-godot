7cb05034f29c989812c4b7ca21daa3914623989d
Kayno0o <kevyn.fyleyssant@gmail.com>
2025-10-19 11:20:57 +0200

    fix: hero stats

--- entities/hero/hero.gd
+++ entities/hero/hero.gd
@@ -8,18 +8,11 @@ class_name Hero extends Mask
 	Enum.EntityType.MaskFarmer: null,
 	Enum.EntityType.MaskTransporter: null,
 }
+@export var respawn_cooldown: float = 20.0
 
 func _ready():
 	super._ready()
 	_on_mask_change(type)
-
-func _on_mask_change(new_type: Enum.EntityType) -> void:
-	type = new_type
-	
-	var animated_sprite: AnimatedSprite2D = sprite
-	animated_sprite.sprite_frames = sprites[new_type]
-
-	state_machine.change_state_type(State.Type.Idle)
 
 func get_health() -> float:
 	return stats[Enum.Stat.Health] + (StatsManager.sum_bonuses(Enum.Stat.Health) / 2)
@@ -29,12 +22,12 @@ func get_inventory_size() -> float:
 func get_pickup_range() -> float:
 	return stats[Enum.Stat.PickupRange] + get_bonus(Enum.Stat.PickupRange) * 2.0
 func get_target_search_cooldown() -> float:
-	return stats[Enum.Stat.TargetSearchCooldown] + get_bonus(Enum.Stat.TargetSearchCooldown) / 2.0
+	return stats[Enum.Stat.TargetSearchCooldown] + get_bonus(Enum.Stat.TargetSearchCooldown) * 2.0
 
 func get_attack() -> float:
 	return stats[Enum.Stat.Attack] + get_bonus(Enum.Stat.Attack) * 2.0
 func get_attack_speed() -> float:
-	return stats[Enum.Stat.AttackSpeed] + get_bonus(Enum.Stat.AttackSpeed) / 2.0
+	return stats[Enum.Stat.AttackSpeed] + get_bonus(Enum.Stat.AttackSpeed) * 2.0
 func get_attack_range() -> float:
 	return stats[Enum.Stat.AttackRange] + get_bonus(Enum.Stat.AttackRange) * 2.0
 func get_attack_view_distance() -> float:
@@ -42,3 +35,11 @@ func get_attack_view_distance() -> float:
 
 func get_movement_speed() -> float:
 	return stats[Enum.Stat.MovementSpeed] + get_bonus(Enum.Stat.MovementSpeed) * 2.0
+
+func _on_mask_change(new_type: Enum.EntityType) -> void:
+	type = new_type
+	
+	var animated_sprite: AnimatedSprite2D = sprite
+	animated_sprite.sprite_frames = sprites[new_type]
+
+	state_machine.change_state_type(State.Type.Idle)
